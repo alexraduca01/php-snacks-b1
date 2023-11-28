7f0765a17c527cfa51d9d682dde99e4e8a2d5a58
alexraduca01 <alex.raduca01@icloud.com>
2023-11-28 14:08:57 +0100

    completed snack_1

--- snack_1/Index.php
+++ snack_1/Index.php
@@ -1,3 +1,34 @@
+<?php 
+
+$games = [
+    [
+        "home" => "inter",
+        "away" => "milan",
+        "homePoints" => 5,
+        "awayPoints" => 1,
+    ],
+    [
+        "home" => "roma",
+        "away" => "lazio",
+        "homePoints" => 2,
+        "awayPoints" => 1,
+    ],
+    [
+        "home" => "juventus",
+        "away" => "napoli",
+        "homePoints" => 3,
+        "awayPoints" => 3,
+    ],
+    [
+        "home" => "atalanta",
+        "away" => "fiorentina",
+        "homePoints" => 4,
+        "awayPoints" => 5,
+    ]
+]
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,10 +36,19 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Snack 1</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     
     <body>
-        
+        <div class="d-flex mt-5 justify-content-center align-items-center fs-1">
+            <ul class="list-unstyled">
+                <?php 
+                foreach ($games as $key => $value) {
+                    echo "<li> {$value['home']} - {$value['away']} {$value['homePoints']} / {$value['awayPoints']} </li>";
+                ?>
+                <?php } ?>
+            </ul>
+        </div>
     </body>
 
 </html>
